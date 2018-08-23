d2d8faff5e0bf66a5babdcce9ab96da1f9683438
Makito <sumimakito@hotmail.com>
2018-08-23 16:48:37 +0800

    Updated the CROS policy of the backend.

--- Backend/server/cors.py
+++ Backend/server/cors.py
@@ -2,5 +2,5 @@ class CorsMiddleware(object):
     def process_response(self, request, response):
         # A dirty fix here, cuz I can't figure out what's wrong with the django-corn-headers dependency.
         # -- Makito
-        response['Access-Control-Allow-Origin'] = '*' # 'stacktodo.mak1t0.cc'
+        response['Access-Control-Allow-Origin'] = 'stacktodo.mak1t0.cc'
         return response
